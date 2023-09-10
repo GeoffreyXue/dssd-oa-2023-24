e09221bcb5fc229f90f7b231c7c307fcb093fef0
GeoffreyXue <xuegeoff@gmail.com>
2023-09-10 15:10:14 -0500

    Add geojson file to backend

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,7 @@ app.use(cors({
 }))
 
 app.get("/", (req, res) => {
-    res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
-    }).status(200)
+    res.sendFile('GeoDS4Bolivia.geojson', { root: '.'})
 })
 
 
--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,35 @@
 const graphDiv = document.getElementById("graph");
 
+// Using https://plotly.com/javascript/mapbox-county-choropleth/
+
 fetch(
-    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+  "https://dssd-oa-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
+  const geojson = await res.json();
+
+  const data = [{
+    type: "choroplethmapbox",
+    featureidkey: "properties.mun",
+    locations: geojson.features.map(f => f.properties.mun),
+    z: geojson.features.map(f => f.properties.pop2020),
+    geojson: geojson,
+    colorbar: { y: 0, yanchor: "bottom", title: { text: "Population in 2020", side: "right" } }
+  }];
+  
+  const layout = {
+    mapbox: {
+      center: { lon: -63, lat: -16 },
+      zoom: 3
+    },
+    width: 600,
+    height: 600
+  };
+  
+  // A local token that is only usable from origin http://localhost:5500
+  const config = {mapboxAccessToken: "pk.eyJ1IjoiZ2VvZmZyZXl4dWUiLCJhIjoiY2xtZHcwM3ZrMW11cDNwbW53a3B3NXV1MiJ9.3N7O0yOCDV38hmlxLtwGWA"};
+  
+  Plotly.newPlot(graphDiv, data, layout, config);
+  
+  
 })
 
